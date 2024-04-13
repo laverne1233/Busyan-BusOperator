039da24777562a11d6344fb55425c3e60a32a519
JuneKyle <62293765+jikisan@users.noreply.github.com>
2024-04-13 19:51:32 +0800

    1st commit

---
+++ CONFIG.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClxrx1JHZKzdnoQpeGU0xdhSe4Szn9LX0",
+    authDomain: "busyan-capstone-3430e.firebaseapp.com",
+    databaseURL: "https://busyan-capstone-3430e-default-rtdb.firebaseio.com",
+    projectId: "busyan-capstone-3430e",
+    storageBucket: "busyan-capstone-3430e.appspot.com",
+    messagingSenderId: "513683055597",
+    appId: "1:513683055597:web:40dc2ff730a1c6b5b07de4",
+    measurementId: "G-NZXE0XTWWH"
+};
+
+export default firebaseConfig;
---
+++ Login/login.js
@@ -0,0 +1,68 @@
+import { DBPaths } from '/Bus Cooperative/js/DB.js';
+import firebaseConfig from '/CONFIG.js';
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+const usernameInput = document.getElementById('usernameInput');
+const passwordInput = document.getElementById('password');
+
+document.getElementById('loginMainForm').addEventListener('submit', loginUser);
+
+function loginUser(event) {
+    event.preventDefault();
+
+    let accountExists = false; // Flag to track if the account exists
+
+    const userRef = database.ref(`${DBPaths.BUS_COOP}`);
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    userRef.once('value', (snapshot) => {
+        snapshot.forEach((user) => {
+            const userKey = user.key;
+            const data = user.val();
+            const dbEmail = data.email; // Access email from user data
+            const dbPassword = data.password; // Access password from user data
+
+            if (username === dbEmail && password == dbPassword) {
+                saveLoginTime(userKey, data);
+                window.location.href = '/Bus Cooperative/bus-coop.html'; // Redirect if credentials match
+                accountExists = true; // Set flag to true if account exists
+                data["key"] = userKey;
+                console.log(data);
+                sessionStorage.setItem('currentUser', JSON.stringify(data));
+                return;
+            }
+
+            return
+        });
+
+        if (!accountExists) {
+            alert('Account does not exist');
+        }
+    });
+}
+
+function saveLoginTime(userId, data) {
+
+    const loginDetailsData = {
+        id: userId,
+        fullName: data.fullName,
+        role: 'cooperative',
+        loginDateTime: new Date().toISOString()
+    }
+
+    const userRef = database.ref(`${DBPaths.LOGIN_NOTIF}`);
+
+    userRef.push(loginDetailsData)
+        .then(() => {
+            hideAddBusCoopModal();
+            // getBusCoop();
+        })
+        .catch(error => {
+            // An error occurred while setting data
+            console.error('Error setting data:', error);
+        });
+}
+
